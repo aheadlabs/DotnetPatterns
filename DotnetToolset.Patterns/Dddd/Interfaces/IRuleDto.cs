73a1568657d04ce216892a0dea660fcab00f2a21
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-08-25 18:36:33 +0200

    Added DTOs related to rules.

---
+++ DotnetToolset.Patterns/Dddd/Interfaces/IRuleDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DotnetToolset.Patterns.Dddd.Interfaces
+{
+	/// <summary>
+	/// Defines a rule
+	/// </summary>
+	public interface IRuleDto : IDto
+
+	{
+		/// <summary>
+		/// Rule key-value pair
+		/// </summary>
+		/// <returns></returns>
+		public KeyValuePair<RuleType, object> Rule { get; set; }
+	}
+}
---
+++ DotnetToolset.Patterns/Dddd/Interfaces/IRulesetSubjectDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace DotnetToolset.Patterns.Dddd.Interfaces
+{
+	/// <summary>
+	/// Defines a subject (object property) that has rules
+	/// </summary>
+	public interface IRulesetSubjectDto : IDto
+	{
+
+
+		/// <summary>
+		/// Gets or sets the subject name
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Rules for this ruleset subject
+		/// </summary>
+		public List<IRuleDto> Rules { get; set; }
+	}
+}
